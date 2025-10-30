cacfe98ecb5e0af9cb8fb26757fa18e46402a622
Mohamed Ibrahim DJIBRILA <djibrilaibrahim14@gmail.com>
2025-10-30 23:41:08 +0100

    fix:Review Demo preview

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// For GitHub Pages, we must serve under a subpath: https://<user>.github.io/<REPO_NAME>/
-// We'll read the base path from the env var BASE_PATH set by CI. Fallback to '/'.
 export default defineConfig({
   plugins: [vue()],
   server: { port: 5173 },
